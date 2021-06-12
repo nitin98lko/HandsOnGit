6295b0497df66d4ce4247b5d29006b4b5802846f
Nitin Roy <nitin98lko@gmail.com>
2021-06-12 15:40:57 +0530

    Add project files.

---
+++ HandsOnGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HandsOnGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
